5dc03e6f0f91f837baca8ce11e707529370b0fdd
Lade Docemo <lade.doc@gmail.com>
2019-10-22 16:32:50 +0100

    Adding the solution to exercise 3

--- exercise_3.rb
+++ exercise_3.rb
@@ -5,12 +5,25 @@
 #      qat   -> atqay
 
 def pig_latin(word)
-
+  a = "q"
+  b = "u"
+  if word.include?(a && b)
+    word << a
+    word << b
+    word = word[2..-1]
+  elsif word.include?(a)
+    word << a
+    word = word[1..-1]
+  else
+    word << b
+    word = word[2..-1]
+  end
+  "#{word}ay"
 end
 
 ## Tests:
 
-require_relative './helpers/assert_equal'
-assert_equal(pig_latin('quick'), 'ickquay')
-assert_equal(pig_latin('quilt'), 'iltquay')
-assert_equal(pig_latin('qat'), 'atqay')
+require_relative "./helpers/assert_equal"
+assert_equal(pig_latin("quick"), "ickquay")
+assert_equal(pig_latin("quilt"), "iltquay")
+assert_equal(pig_latin("qat"), "atqay")
