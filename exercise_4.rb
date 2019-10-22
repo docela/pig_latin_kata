7a4b9fc82428e61bf85df396fa6aaca318b6f253
Lade Docemo <lade.doc@gmail.com>
2019-10-22 16:32:05 +0100

    Adding solution to exercise 4

--- exercise_4.rb
+++ exercise_4.rb
@@ -6,11 +6,27 @@
 # other formats
 
 def pig_latin(word)
+# This sets the initial word first as a separate variable and also it's first index as a variable
+  x = word
+  first_letter = word[0]
 
+# This flips the consonants to the end of the word
+  until ["a", "e", "i", "o", "u"].include?(first_letter)
+    word << first_letter
+    word = word[1..-1]
+    first_letter = word[0]
+  end
+# This if statement selects the initial/ any word who has a capital
+  if x[0] == x[0].capitalize
+    word.downcase!
+    word.capitalize!
+  end
+# Then prints the word
+  "#{word}ay"
 end
 
 ## Tests:
 
-require_relative './helpers/assert_equal'
-assert_equal(pig_latin('Dog'), 'Ogday')
-assert_equal(pig_latin('cram'), 'amcray')
+require_relative "./helpers/assert_equal"
+assert_equal(pig_latin("Dog"), "Ogday")
+assert_equal(pig_latin("cram"), "amcray")
